7a0421b90966f9bc83ae8cc5704e31f9a0465f99
Manh Linh <mlinhalone@gmail.com>
2025-11-08 22:43:27 +0700

    Bài 31

---
+++ Chapter1/Lesson31.js
@@ -0,0 +1,13 @@
+const checkSoNguyenTo = (n) => {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(checkSoNguyenTo(2));
