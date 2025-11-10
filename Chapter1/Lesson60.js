57949aaf0421a196d608eec768b0349d0b2543d7
Manh Linh <mlinhalone@gmail.com>
2025-11-10 10:01:43 +0700

    Bài 60

---
+++ Chapter1/Lesson60.js
@@ -0,0 +1,13 @@
+const checkChuSoTangDan = (n) => {
+  const nums = String(n).split("");
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] >= nums[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+console.log(checkChuSoTangDan(1234956789));
