d635423a8494ed00fab4fd06f0a24841578bc92a
Manh Linh <mlinhalone@gmail.com>
2025-11-10 22:36:51 +0700

    Bài 65

---
+++ Chapter1/Lesson65.js
@@ -0,0 +1,22 @@
+const phuongTrinhBac2 = (a, b, c) => {
+  let delta = b * b - 4 * a * c;
+  if (a === 0) {
+    if (b !== 0) {
+      return -c / b;
+    }
+    if (c !== 0) return "Vô nghiệm";
+    return "Vô số nghiệm";
+  }
+
+  if (delta > 0) {
+    return `x1 = ${(-b + Math.sqrt(delta)) / (2 * a)} x2 = ${
+      (-b - Math.sqrt(delta)) / (2 * a)
+    }`;
+  } else if (delta === 0) {
+    return `x1 = x2 = ${-b / (2 * a)}`;
+  } else {
+    return "Vô nghiệm";
+  }
+};
+
+console.log(phuongTrinhBac2(1, 2, 1));
